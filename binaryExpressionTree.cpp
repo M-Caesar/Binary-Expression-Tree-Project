e132d933665ff7c775ccb792a71d1d3fbebea7ae
Nicholas Augustus <na50324@email.vccs.edu>
2024-04-10 19:40:58 -0400

    Changes made

--- binaryExpressionTree.cpp
+++ binaryExpressionTree.cpp
@@ -5,23 +5,70 @@ void buildExpressionTree(string pfinput)
 {
 	//This class is a template, so the Type parameter should
 	//be specified as a pointer to a nodeType<stromg struct>
-	stack<string> binaryTreeNodes;
+	stack<nodeType<string>*> binaryTreeNodes;
 
 	string postfix = pfinput;
+	nodeType<string>* newdigitNode = new nodeType<string>;
+	nodeType<string>* newopNode = new nodeType<string>;
+	nodeType<string>* rootNode = new nodeType<string>;
 
 	//convert the string into an array of chars
 	char* expression = new char[postfix.length() + 1];
-	strcpy(expression, postfix.c_str());
-	for (char c : postfix)
+	char* c = strcpy(expression, postfix.c_str());
+	char* c = strtok(expression, postfix.c_str());
+
+
+	//change to do while loop
+	//for (char c : postfix)
+	do
 	{
-		if (isdigit(c))
+		if (isdigit(c[0]))
+		{ //how to create new node?
+			string(c);
+			//store the string onto the info field
+			newdigitNode->info = c;
+			//push the new node onto the stack
+			binaryTreeNodes.push(newdigitNode);
+		}
+		else
 		{
-			node = new node<string>; //how to create new node?
+			newopNode->info = c;
+			if (!binaryTreeNodes.empty())
+			{
+				nodeType<string>* ref;
+				ref = binaryTreeNodes.top();
+				newopNode->rLink = ref;
+				binaryTreeNodes.pop();
+				if (!binaryTreeNodes.empty())
+				{
+					//nodeType<string>* refL;
+					ref = binaryTreeNodes.top();
+					newopNode->lLink = ref;
+					binaryTreeNodes.pop();
+					binaryTreeNodes.push(newopNode);
+				}
+				else
+					std::cout << "Error - Stack is empty" << endl;
+			}
+			else 
+				std::cout << "Error - Stack is empty" << endl;
+			
+		}
+	} while (c = strtok(NULL, " "));
+	if (!binaryTreeNodes.empty())
+	{
+		rootNode = binaryTreeNodes.top();
+		binaryTreeNodes.pop();
+		if (!binaryTreeNodes.empty())
+		{
+			std::cout << "There was an error. Setting root to null" << std::endl;
+			rootNode = nullptr;
 		}
 	}
+	//while (token = strcpy(NULL, " "));
 }
 double evaluateExpressionTree();
-double evaluateExpressionTree(noteType<string>*);
+//double evaluateExpressionTree(noteType<string>*);
 bool search(const string&);
 void insert(const string&);
 void deleteNode(const string&);
